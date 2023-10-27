d15607953660e3c61e13a41423fd1755aff0d3de
Luke Berry <lukeberrypi@gmail.com>
2023-10-27 01:11:21 -0300

    initial commit

---
+++ app/_layout.tsx
@@ -0,0 +1,15 @@
+// app/_layout.tsx
+import { Stack } from "expo-router";
+import { NativeWindStyleSheet } from "nativewind";
+
+NativeWindStyleSheet.setOutput({
+  default: "native",
+});
+
+export const unstable_settings = {
+  initialRouteName: "index",
+};
+
+export default function Layout() {
+  return <Stack />;
+}
